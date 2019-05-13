ad9c4ff5e30ea4d3091fcc3fc0aca4934d57812f
endyranaudo <endyranaudo@gmail.com>
2019-05-13 17:11:30 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  changeText();
+});
+
+function changeText() {
+  document.getElementById("text").innerHTML = "This is really cool!"
+}
