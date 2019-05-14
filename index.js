f0563c3d033d2c3ace494dce7d5f4078df18af52
KevinsVision <us.kevin@icloud.com>
2019-05-14 16:56:31 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    newText()
+});
+
+function newText() {
+    document.getElementById('text')
+        .innerHTML = "This is really cool!";
+}
